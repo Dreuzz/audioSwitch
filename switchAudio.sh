c0d30f001e5262c1dcae3a3577a98b795dae3dd3
dreuzz <dreuzzz@proton.me>
2024-09-29 02:05:14 +0200

    s

--- switchAudio.sh
+++ switchAudio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-current_dir=${PWD}
+current_dir=${PWD}/assets/gear.svg
 
 # Get the current default sink
 current_sink=$(pactl get-default-sink)
@@ -9,11 +9,11 @@ current_sink=$(pactl get-default-sink)
 if [ "$current_sink" == "alsa_output.pci-0000_00_1f.3.analog-stereo" ]; then
     pactl set-default-sink alsa_output.pci-0000_03_00.1.hdmi-stereo-extra2
     echo "Switched to HDMI output."
-    notify-send "Switched to HDMI output." -i $current_dir/assets/gear.svg
+    notify-send "Switched to HDMI output." -i $current_dir
 elif [ "$current_sink" == "alsa_output.pci-0000_03_00.1.hdmi-stereo-extra2" ]; then
     pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo
     echo "Switched to analog output."
-    notify-send "Switched to analog output." -i $current_dir/assets/gear.svg
+    notify-send "Switched to analog output." -i $current_dir
 else
     echo "Unknown default sink: $current_sink"
 fi
